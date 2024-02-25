bdad417c34744d27ac6fb605c5bbeb074ae360e5
Heri Setyawan <herisetyawan233@gmail.com>
2024-02-25 11:12:38 +0700

    fixup: error when build because escaoe

--- src/components/molekul/information/stat.tsx
+++ src/components/molekul/information/stat.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Stats() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 border-b-[1px] border-white ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 border-b-[1px] border-white">
     <div className='px-3 py-20 flex gap-8 justify-center items-center'>
        <h1 className='font-extrabold text-6xl'>11K+</h1>
        <p className='text-slate-400'>Happy Customer with our services</p>
